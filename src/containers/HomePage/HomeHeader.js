2b9fff1dc47e6e2c6098594f87f52c662aad681c
DTinh <duytinh10833@gmail.com>
2024-10-09 20:10:44 +0700

    frontend-reactjs

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -81,7 +81,7 @@ class HomeHeader extends Component {
                                 <div className='text-child'><FormattedMessage id="banner.child3" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className="fas fa-microscope"></i></div>
+                                <div className='icon-child'><i class="fas fa-vial"></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child4" /></div>
                             </div>
                             <div className='option-child'>
@@ -89,7 +89,7 @@ class HomeHeader extends Component {
                                 <div className='text-child'><FormattedMessage id="banner.child5" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i class="fas fa-tooth"></i></div>
+                                <div className='icon-child'><i class="fas fa-notes-medical"></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child6" /></div>
 
                             </div>
